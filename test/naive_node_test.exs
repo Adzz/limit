faded9ef82ad12388297722bb21a43ff8fb8b5c8
Adz <email@test.example>
2019-07-01 20:30:43 +0100

    Make the sparingly few tests actually pass

--- test/naive_node_test.exs
+++ test/naive_node_test.exs
@@ -1,9 +1,9 @@
-defmodule NaiveNodeTest do
+defmodule NaiveTest do
   use ExUnit.Case, async: true
 
   describe "increment/1" do
     test "increments the value for the given node" do
-      assert NaiveNode.increment(%NaiveNode{id: 1, state: 0}) == %NaiveNode{id: 1, state: 1}
+      assert CVRDT.increment(%Naive{id: 1, state: 0}) == %Naive{id: 1, state: 1}
     end
   end
 end
--- test/node_test.exs
+++ test/node_test.exs
@@ -1,9 +1,9 @@
-defmodule NodeTest do
+defmodule VectorIntsTest do
   use ExUnit.Case
 
   describe "increment/1" do
     test "increments the value for the given node" do
-      assert Node.increment(%Node{id: 1, node_states: %{1 => 0}}) == %Node{
+      assert CVRDT.increment(%VectorInts{id: 1, node_states: %{1 => 0}}) == %VectorInts{
                id: 1,
                node_states: %{1 => 1}
              }
@@ -14,16 +14,19 @@ defmodule NodeTest do
     test "sums all of the values in node_states" do
       node_states = %{1 => 15, 2 => 7}
 
-      assert Node.system_value(%Node{id: 1, node_states: node_states}) == 22
+      assert CVRDT.value(%VectorInts{id: 1, node_states: node_states}) == 22
     end
   end
 
-  describe "merge/2" do
+  describe "join/2" do
     test "takes the co-ordinate wise max of the two node states and returns that" do
-      node_1 = %Node{id: 1, node_states: %{1 => 15, 2 => 7}}
-      node_2 = %Node{id: 2, node_states: %{1 => 15, 2 => 8}}
+      node_1 = %VectorInts{id: 1, node_states: %{1 => 15, 2 => 7}}
+      node_2 = %VectorInts{id: 2, node_states: %{1 => 15, 2 => 8}}
 
-      assert Node.merge(node_1, node_2) == %Node{id: 1, node_states: %{1 => 15, 2 => 8}}
+      assert CVRDT.join(node_1, node_2) == %VectorInts{
+               id: 1,
+               node_states: %{1 => 15, 2 => 8}
+             }
     end
   end
 end
